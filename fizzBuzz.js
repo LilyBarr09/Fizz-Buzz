4e40a049e1259f97ba61cb892afa2ca371cf7643
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-09-06 15:39:33 -0400

    Fizz Buzz challenge in Typescript

---
+++ fizzBuzz.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+function fizzBuzz() {
+    var i = 100;
+    for (var i_1 = 0; i_1 < 100; i_1++) {
+        if (i_1 % 3 === 0 && i_1 % 5 === 0) {
+            console.log("Fizz Buzz");
+        }
+        else if (i_1 % 3 === 0) {
+            console.log("Fizz");
+        }
+        else if (i_1 % 5 === 0) {
+            console.log("Buzz");
+        }
+    }
+    return i;
+}
+//# sourceMappingURL=fizzBuzz.js.map
